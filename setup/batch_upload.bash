d0921575756c8fb2b7d086037ba84429c9f9586a
Lucas <lduncan1712@gmail.com>
2025-02-22 22:42:35 -0500

    Plots

--- setup/batch_upload.bash
+++ setup/batch_upload.bash
@@ -9,8 +9,8 @@ python -m setup.upload --path "data\\Instagram\\your_instagram_activity\\message
 python -m setup.upload --path "data\\Message"
 
 #Uploading (Time Split --> Participant Split (Call) Files)
-python multi_to_single_preprocessing\\join_files_call.py --input "data\\Call" --output "data\\joined_calls.csv"
-python multi_to_single_preprocessing\\split_file_call.py --input "data\\joined_calls.csv" --output "data\\participant_split_calls"
+python restructuring\\join_files_call.py --input "data\\Call" --output "data\\joined_calls.csv"
+python restructuring\\split_file_call.py --input "data\\joined_calls.csv" --output "data\\participant_split_calls"
 python -m setup.upload --path "data\\participant_split_calls"
 
 #Uploading Contacts (Optional)
